eee2fcc2e914434cac604227e0bbcfb687a55827
Damion Russell <domino.russell@gmail.com>
2022-08-04 13:08:03 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
-
 // Exercise 1
-
+const firstName = "Damion";
+let lastName = "Russell";
+var age = 23;
 // Exercise 2
-
+let fullName = firstName + " " + lastName;
+console.log(fullName);
 // Exercise 3
+let city = "Wooster";
+let passTime = "juggling";
+let myStory = "My name is " + fullName + " I live in " + city + " and my favorite pass time is " + passTime;
+console.log(myStory)
